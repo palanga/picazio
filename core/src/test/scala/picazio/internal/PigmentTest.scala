b22a4716c71a5cafabe27541f4b063aaf4029830
Andrés González <a.gonzalez.terres@gmail.com>
2024-03-24 19:30:51 -0300

    wip color

--- core/src/test/scala/picazio/internal/PigmentTest.scala
+++ core/src/test/scala/picazio/internal/PigmentTest.scala
@@ -6,14 +6,14 @@ import org.scalatest.matchers.should.Matchers
 class PigmentTest extends AnyFunSuiteLike with Matchers {
 
   // pantone 7481 -> 00B74F
-  private val verdeRgb = Pigment.RGB(0, 183, 79)
-  private val verdeHsl = Pigment.HSL(146, 100, 36)
+  private val verdeAbortoLegalRgb = Pigment.RGB(0, 183, 79)
+  private val verdeAbortoLegalHsl = Pigment.HSL(146, 100, 36)
 
   test("pigment from hex string lower case with numeral char") {
     Pigment.fromHexStringUnsafe("#00b74f") match {
       case rgb: Pigment.RGB =>
-        rgb shouldBe verdeRgb
-        Pigment.rgbToHsl(rgb) shouldBe verdeHsl
+        rgb shouldBe verdeAbortoLegalRgb
+        Pigment.rgbToHsl(rgb) shouldBe verdeAbortoLegalHsl
       case _                => ??? // shouldn't happen
     }
   }
@@ -21,8 +21,8 @@ class PigmentTest extends AnyFunSuiteLike with Matchers {
   test("pigment from hex string upper case with numeral char") {
     Pigment.fromHexStringUnsafe("#00B74F") match {
       case rgb: Pigment.RGB =>
-        rgb shouldBe verdeRgb
-        Pigment.rgbToHsl(rgb) shouldBe verdeHsl
+        rgb shouldBe verdeAbortoLegalRgb
+        Pigment.rgbToHsl(rgb) shouldBe verdeAbortoLegalHsl
       case _                => ??? // shouldn't happen
     }
   }
@@ -30,8 +30,8 @@ class PigmentTest extends AnyFunSuiteLike with Matchers {
   test("pigment from hex string lower case with 0x") {
     Pigment.fromHexStringUnsafe("0x00b74f") match {
       case rgb: Pigment.RGB =>
-        rgb shouldBe verdeRgb
-        Pigment.rgbToHsl(rgb) shouldBe verdeHsl
+        rgb shouldBe verdeAbortoLegalRgb
+        Pigment.rgbToHsl(rgb) shouldBe verdeAbortoLegalHsl
       case _                => ??? // shouldn't happen
     }
   }
@@ -40,8 +40,8 @@ class PigmentTest extends AnyFunSuiteLike with Matchers {
 
     Pigment.fromHexStringUnsafe("00b74f") match {
       case rgb: Pigment.RGB =>
-        rgb shouldBe verdeRgb
-        Pigment.rgbToHsl(rgb) shouldBe verdeHsl
+        rgb shouldBe verdeAbortoLegalRgb
+        Pigment.rgbToHsl(rgb) shouldBe verdeAbortoLegalHsl
       case _                => ??? // shouldn't happen
     }
   }
@@ -52,27 +52,27 @@ class PigmentTest extends AnyFunSuiteLike with Matchers {
   }
 
   test("lighter should increase lightness by 10%") {
-    verdeRgb.lighter shouldBe Pigment.HSL(146, 100, 46)
+    verdeAbortoLegalRgb.lighter shouldBe Pigment.HSL(146, 100, 46)
   }
 
   test("lighter should increase lightness until 100%") {
-    verdeRgb.lighter.lighter.lighter.lighter.lighter.lighter.lighter shouldBe Pigment.HSL(146, 100, 100)
+    verdeAbortoLegalRgb.lighter.lighter.lighter.lighter.lighter.lighter.lighter shouldBe Pigment.HSL(146, 100, 100)
   }
 
   test("darker should decrease lightness by 10%") {
-    verdeRgb.darker shouldBe Pigment.HSL(146, 100, 26)
+    verdeAbortoLegalRgb.darker shouldBe Pigment.HSL(146, 100, 26)
   }
 
   test("darker should decrease lightness until 0%") {
-    verdeRgb.darker.darker.darker.darker shouldBe Pigment.HSL(146, 100, 0)
+    verdeAbortoLegalRgb.darker.darker.darker.darker shouldBe Pigment.HSL(146, 100, 0)
   }
 
   test("rgb to string") {
-    verdeRgb.toString shouldBe "rgb(0, 183, 79)"
+    verdeAbortoLegalRgb.toString shouldBe "rgb(0, 183, 79)"
   }
 
   test("hsl to string") {
-    verdeHsl.toString shouldBe "hsl(146, 100%, 36%)"
+    verdeAbortoLegalHsl.toString shouldBe "hsl(146, 100%, 36%)"
   }
 
 }
